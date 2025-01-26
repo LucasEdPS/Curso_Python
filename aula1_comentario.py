316f08c91ece900c32a80b5010c3a25c179d0b25
Lucas <160774870+LucasEdPS@users.noreply.github.com>
2025-01-26 01:32:16 -0300

    Aula 1 a 25
    atividades das aula praticas 01 a 25 que correspondem até a aula 44 do curso

---
+++ aula1_comentario.py
@@ -0,0 +1,13 @@
+# Cerquilha pode ser usada para fazer comentários
+print(123) # ou aqui
+'''
+Não é exatamente um comentario
+mas pode ser usado para escrever algo que o programa
+vai ler mas não irá interpretar
+e utilizando multiplas linhas
+'''
+print(456)
+"""
+Pode ser utilizando também
+outro tipo de aspas
+"""
---
+++ aula2_print.py
@@ -0,0 +1,7 @@
+print(123)
+print(1,2,3)
+print(4,5,6, sep='-')
+print(7,8,9, sep='*', end='.\n inicio ')
+print(10,11,12, sep='/', end='-')
+print(13,14,15, sep='/', end=' fim \n')
+
